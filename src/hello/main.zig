bb76a2a3f53955457c40b11572aa915fdfad34f1
smallkirby <ssmallkirby@gmail.com>
2023-09-13 23:28:20 +0900

    hello: update
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- src/hello/main.zig
+++ src/hello/main.zig
@@ -1,6 +1,14 @@
-const std = @import("std");
+//! This is a top-level doc comment.
+//! And this is a second line.
 
+const std = @import("std");
+const print = @import("std").debug.print;
+const log = @import("std").log;
+
+/// Hello main function
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     try stdout.print("Hello, {s}!\n", .{"World"});
+    print("Hello from std.debug.print...!\n", .{});
+    log.info("Hello from std.log.info...!", .{});
 }
